375f9c99950922c0f49c6d54a639c3dfadc15dea
shahar-gad <shahar.gad.shriki@traxretail.com>
2020-12-21 13:53:16 +0200

    try python

---
+++ file1.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'a':[],'b':[]})
+
+# Markdown test
+
+df.head()
+df.head()
